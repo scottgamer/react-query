f9b058774d7bd3a8322dbc955b473ac2d434c570
rmunoz <richosojason@msn.com>
2021-08-02 13:00:04 -0500

    added error handling

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -17,7 +17,20 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery("posts", fetchPosts);
+  const { data, error, isError, isLoading } = useQuery("posts", fetchPosts);
+
+  if (isLoading) {
+    return <h3>Loading</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>There was an error fetching the data</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
