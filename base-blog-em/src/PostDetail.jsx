0f000365ead6d9344859bdd5c6ddc9d569f7f853
rmunoz <richosojason@msn.com>
2021-08-03 15:36:39 -0500

    added useQuery for comments

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,22 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, error, isLoading, isError } = useQuery("comments", () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
