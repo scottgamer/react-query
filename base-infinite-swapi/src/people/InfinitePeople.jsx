b803849bd9934694a8140725546c37a71e1776ab
rmunoz <richosojason@msn.com>
2021-08-05 11:31:12 -0500

    added loading and error state for infinite scroll

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -3,7 +3,7 @@ import { useInfiniteQuery } from "react-query";
 
 import { Person } from "./Person";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = "https://swapi-deno.azurewebsites.net/api/people/";
 
 const fetchUrl = async (url) => {
   const response = await fetch(url);
@@ -11,24 +11,43 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
     "sw-people",
     ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     { getNextPageParam: (lastPage) => lastPage.next || undefined }
   );
 
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>Error {error.toString()}</h3>;
+  }
+
   return (
-    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
-      {data.pages.map((pageData) =>
-        pageData.results.map((person) => (
-          <Person
-            key={person.name}
-            name={person.name}
-            hairColor={person.hair_color}
-            eyeColor={person.eye_color}
-          />
-        ))
-      )}
-    </InfiniteScroll>
+    <>
+      {isFetching && <h3>Loading...</h3>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) =>
+          pageData.map((person) => (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
   );
 }
