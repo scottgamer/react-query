48336c0b1db3fbf0536a5e33bd9492f210e8b585
rmunoz <richosojason@msn.com>
2021-08-17 22:44:53 -0500

    added query client provider to index

--- base-lazy-days/client/src/index.tsx
+++ base-lazy-days/client/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { App } from './components/app/App';
+import { queryClient } from './react-query/queryClient';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
