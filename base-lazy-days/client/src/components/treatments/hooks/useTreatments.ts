e2de1398bddba34312ab3ddf6c574fc71c32d051
rmunoz <richosojason@msn.com>
2021-08-17 23:00:04 -0500

    added error message to toast

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,7 +12,16 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  // const fallback = [];
-  const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
+  const toast = useCustomToast();
+
+  const { data = [] } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.toString().replace(/^Error:\s*/, '')
+          : 'Error connecting to the server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
