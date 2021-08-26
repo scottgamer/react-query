4adbbf3ec73aacee08db31f868a9ccce601d4f36
rmunoz <richosojason@msn.com>
2021-08-26 16:45:40 -0500

    removed custom error handler from treatments hook

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,16 +11,6 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
-
-  const { data = [] } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title =
-        error instanceof Error
-          ? error.toString().replace(/^Error:\s*/, '')
-          : 'Error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
