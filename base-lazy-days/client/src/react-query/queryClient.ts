fd39799e7457b94f64a77153d0c5add7f4e8bab5
rmunoz <richosojason@msn.com>
2021-08-26 16:45:23 -0500

    added default error handler to query client

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,21 +1,28 @@
+import { createStandaloneToast } from '@chakra-ui/react';
 import { QueryClient } from 'react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+import { theme } from '../theme';
 
-// export function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const id = 'react-query-error';
-//   const title =
-//     error instanceof Error
-//       ? // remove the initial 'Error: ' that accompanies many errors
-//         error.toString().replace(/^Error:\s*/, '')
-//       : 'error connecting to server';
+const toast = createStandaloneToast({ theme });
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+export function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const id = 'react-query-error';
+  const title =
+    error instanceof Error
+      ? // remove the initial 'Error: ' that accompanies many errors
+        error.toString().replace(/^Error:\s*/, '')
+      : 'error connecting to server';
 
-export const queryClient = new QueryClient();
+  // prevent duplicate toasts
+  toast.closeAll();
+  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
