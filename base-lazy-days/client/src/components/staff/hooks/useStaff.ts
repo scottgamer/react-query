06e42e4bd0026e41c20ec1f9b7a90ae36128ccac
rmunoz <richosojason@msn.com>
2021-09-07 23:08:11 -0500

    added select to use staff hook

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,10 +1,10 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-// import { filterByTreatment } from '../utils';
+import { filterByTreatment } from '../utils';
 
 async function getStaff(): Promise<Staff[]> {
   const { data } = await axiosInstance.get('/staff');
@@ -20,9 +20,15 @@ interface UseStaff {
 export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
+  const selectFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
 
-  // TODO: get data from server via useQuery
-  const { data: staff = [] } = useQuery(queryKeys.staff, getStaff);
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter !== 'all' ? selectFn : undefined,
+  });
 
   return { staff, filter, setFilter };
 }
