ea20ae46b4cd3c0a719c5c8ea7baf169bd917721
rmunoz <richosojason@msn.com>
2021-11-15 15:29:09 -0500

    added renderWithQueryClient

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,4 +1,19 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// make a function to generate a unique query client for each test
+const generateQueryClient = () => new QueryClient();
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateQueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
